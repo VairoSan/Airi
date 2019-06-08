66c2f914e96552bdeb0375e5415e3d8caf47b46b
VairoSan <vairo.man380@gmail.com>
2019-06-08 08:11:34 +0300

    Handling commands

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const { Client, Collection } = require('discord.js');
 const winston = require('winston');
-const { token } = require('./config.json');
+const commands = require('./commands/');
+const { token, prefix } = require('./config.json');
 
 const client = new Client({ disableEveryone: true });
 const logger = winston.createLogger({
@@ -14,7 +15,27 @@ const logger = winston.createLogger({
 
 client.once('ready', () => {
   client.commands = new Collection();
-  logger.debug(`connected as ${client.user.tag}`);
+  logger.info(`connected as ${client.user.tag}`);
+});
+
+client.on('message', msg => {
+  // ignore bots and messages that not start with the prefix
+  if (msg.author.bot || !msg.content.startsWith(prefix)) return undefined;
+  
+  // handling commands
+  const args = msg.content.slice(prefix.length).split(/ +/);
+  const commandName = args.shift().toLowerCase();
+  const commandsKeys = Object.keys(commands);
+
+  try {
+    if (commandsKeys.includes(commandName)) commands[commandName](msg, args);
+    else msg.reply('Invalid command!');
+
+  } catch (ex) {
+    msg.reply('Unexpected error happened while running this command.');
+    logger.error(ex);
+  }
+
 });
 
 
