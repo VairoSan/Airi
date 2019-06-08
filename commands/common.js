ffa96007ccd3d3a3324dbccbfe913ab6922d3b97
VairoSan <vairo.man380@gmail.com>
2019-06-08 08:13:22 +0300

    commands entry file

---
+++ commands/common.js
@@ -0,0 +1,9 @@
+// all the common commands 
+
+function ping(msg, args) {
+  return msg.reply('Pong');
+}
+
+module.exports = {
+  ping
+};
---
+++ commands/index.js
@@ -0,0 +1,4 @@
+// import all commands here and return them
+const commonCommands = require('./common');
+
+module.exports = { ...commonCommands };
